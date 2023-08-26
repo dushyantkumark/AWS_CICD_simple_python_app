be3ca985429e29d664795bd9f311e81e520949c1
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2023-08-27 01:35:45 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull dushyantkumark/simple-python-flask-app
 
-# Run the Docker image as a container
+# Run the Docker image as a container 
 docker run -d -p 5000:5000 dushyantkumark/simple-python-flask-app
