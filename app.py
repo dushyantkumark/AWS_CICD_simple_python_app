2fcab0ce93b64966828c929214ccf3f10eab9952
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2023-08-26 12:06:20 +0530

    Update app.py
    new updaate

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'This is AWS CICD Pipeline Automation'
+    return 'Hello, This is AWS CICD Pipeline Automation'
 
 if __name__ == '__main__':
     app.run()
